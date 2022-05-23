84db774902e4852eab593dad092f91616b3e8c1d
Brandon Minter <bsm@duostech.com>
2022-05-23 16:52:30 -0400

    adding functionality to add wsdl response to our records

---
+++ src/wsdl_receive.rs
@@ -0,0 +1,6 @@
+/* 
+* This module defines the Async function that will listen via tcp/soap/wsdl for a response from the umler webservice and a function to format this information before it 
+* is passed back to the dbInterface module and added to our car details table.
+
+The add function in dbInterface.rs  is the mock up of what is to be done in this module
+*/
