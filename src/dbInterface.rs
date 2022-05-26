674f4e5d8584a2af1db178fc0d5b434468e5c168
Brandon Minter <bsm@duostech.com>
2022-05-26 14:16:20 -0400

    working mysql schema passing

--- src/dbInterface.rs
+++ src/dbInterface.rs
@@ -37,34 +37,50 @@ const DB_REF_FILE: &str = "db_ref.txt";
 pub fn run() {
     let db_url = get_db_url();
     let mut current_ID = "".to_string();
-    println!("\nRun func: ID --> {:?}", current_ID.clone());
+    println!("\nStarting sequence...\n");
     let mut current_connection = db_connection().unwrap();
-    println!("\nRun func: conn --> {:?}", &current_connection);
+    println!("\nConnection established...\n");
     let unknown_car_table = get_unknown_ID_table();
-    println!("\nRun func: unknown table --> {}", unknown_car_table.clone());
+    println!("\nRun func: unknown table --> {}\n", unknown_car_table.clone());
     let car_details_table = get_car_details_table();
-    println!("\nRun func: detail table --> {}", car_details_table.clone());
-    let unk_stmt = prep_unknown_Id_query(unknown_car_table.trim().to_string());
-    println!("\nRun func: stmt --> {}", unk_stmt.clone());
+    println!("\nRun func: detail table --> {}\n", car_details_table.clone()); 
     let last_searched = get_last_unknown();
-    println!("\nRun func: last--> {}", last_searched.clone());
-    let current_schema = get_table_schema(current_connection).unwrap(); //not returning anything need to tweak return type for this when vec is created
-    for i in current_schema.iter() {println!("\nname: {:?}", i);}
-    println!("Run func: Current Schema {:?}", current_schema); //see above comment!!
+    println!("\nRun func: last ID sent to Umler--> {}\n", last_searched.clone());
+    let (current_schema, counter) = get_table_schema(current_connection).unwrap(); 
+    for i in current_schema.iter() {println!("\nname: {:?}\n", i);} //same iteration as schema, outside the function...
+    println!("\nRun func: Current Schema ---> {:?}\n", current_schema);      //same info as line above, just as blob and not iterated...
+
     let current_connection = db_connection().unwrap();
     let current_connection2 = db_connection().unwrap();
     let current_connection3 = db_connection().unwrap();
-    // println!("updated connection is: \n {:?}", &current_connection);
+
+    let unk_stmt = prep_unknown_Id_query(unknown_car_table.trim().to_string());
+    println!("\nRun func: stmt --> {}\n", unk_stmt.clone());
     let unknown_car_IDs = scrub_unknowns(current_connection, unk_stmt);
-    println!("\nRun func: car ID's{:?}", unknown_car_IDs.clone());
-    if unknown_car_IDs.len() >= 2 {
-        for i in unknown_car_IDs.iter() {webservice_formatter(i.clone()); println!("\ni is currently: {}\n", i); } 
+    println!("\nRun func: car ID's ---> {:?}\n", unknown_car_IDs.clone());
+
+
+    //up to this point everything works fine...shit gets hairy after this...
+    
+
 
     let wsdl_stmt = wsdl_send::db_statement_formatter(unknown_car_IDs.clone());
-    let wsdl_response = wsdl_send::dummy_wsdl_send(current_connection2, wsdl_stmt, db_url);
-    add(current_connection3, wsdl_response.unwrap(), car_details_table);
+    let wsdl_response = wsdl_send::dummy_wsdl_send(current_connection2, wsdl_stmt, db_url, counter).unwrap();
 
-    println!("\nRun func: Unknown Cars {:?}", unknown_car_IDs.clone()); 
+    // for i in wsdl_response {                //WORKING!!
+    //     println!("\n\n\n********This is what is returned from the Dummy WSDL call, displayed from dbInterface module********\n\n\n");
+    //     println!("{}", i);
+    // }
+    //add(current_connection3, wsdl_response.unwrap(), car_details_table);
+
+
+
+
+    /*
+    The code below is the next step once MySQL schema problem is resolved and understood code below (webservice formatter) is meant to format request to umler...
+    */
+    // if unknown_car_IDs.len() >= 2 {        
+    //     for i in unknown_car_IDs.iter() {webservice_formatter(i.clone()); println!("\ni is currently: {}\n", i); } 
     println!("\nEnd of run function...\n");
 }
 
@@ -105,7 +121,7 @@ pub fn webservice_formatter(current_ID:String) {
         settings::logthis_webService(Errornote, webservice_comm_type.clone());
 
     } else {
-        let lognote = format!("Current unknown car ID is-----> {}", current_ID);
+        let lognote = format!("Current unknown car ID formatting is-----> {}", current_ID);
         println!("{}", lognote);
         settings::logthis_nonError(lognote);
     }
@@ -135,6 +151,7 @@ pub fn get_car_details_table () -> String {
     let test_message = "can't add to db because ID was blank".to_string();
     let mut holder = String::new();
     let mut reader = BufReader::new(db_reference);
+
     let db_string = reader.read_line(&mut holder);
     holder.clear();
     let db_string = reader.read_line(&mut holder);
@@ -142,9 +159,8 @@ pub fn get_car_details_table () -> String {
     let db_string = reader.read_line(&mut holder);
     holder.clear();
     let db_string = reader.read_line(&mut holder);
-    // println!("get_car_details_table func: Current length in bytes is: {:?}, the value in db-string is: {:?}", db_string.unwrap(), holder);
+
     let sel_tables_as_str = str::replace(&holder, "-Car Detail Storage Table", "");
-    println!("get_car_details_table func: formatted is: {:?}\n", sel_tables_as_str.trim());
     let return_val = sel_tables_as_str.to_owned();
     return_val.to_string()
 }
@@ -153,22 +169,23 @@ pub fn get_unknown_ID_table () -> String {
     let mut db_reference = OpenOptions::new().read(true).write(true).open(DB_REF_FILE).unwrap();
     let mut holder = String::new();
     let mut reader = BufReader::new(db_reference);
+
     let db_string = reader.read_line(&mut holder);
     holder.clear();
     let db_string = reader.read_line(&mut holder);
     holder.clear();
     let db_string = reader.read_line(&mut holder);
-    // println!("get_unknown_ID_table func: Current length in bytes is: {:?}, the value in db-string is: {:?}", db_string.unwrap(), holder.trim());
+
     let sel_tables_as_str = str::replace(&holder, "-Unknown Car ID Table", "");
-    println!("\nget_unknown_ID_table func: formatted is: {}\n", sel_tables_as_str.trim());
     sel_tables_as_str
 }
 
-pub fn get_table_schema (current_connection: PooledConn) -> Result<Vec<String>, Error> {
+pub fn get_table_schema (current_connection: PooledConn) -> Result<(Vec<String>, i32), Error> {
+    println!("\n\n\n\nThe following lines are the Schema Pulled from our car details table....so we know the needed format for info received back from umler\n\n\n\n");
     let mut conn = current_connection;
     let car_details_table_pass = get_car_details_table();
     let get_schema_stmt = format!("SHOW COLUMNS IN {}", car_details_table_pass.trim());
-    println!("The Schema statement---> {}\n", get_schema_stmt);
+    //println!("The Schema statement---> {}\n", get_schema_stmt);
     let mut return_vec:Vec<String>= vec![];
     let mut selection = conn.start_transaction(TxOpts::default())?;
     let res:Vec<Row> = selection.query(get_schema_stmt).unwrap();
@@ -218,8 +235,9 @@ pub fn get_table_schema (current_connection: PooledConn) -> Result<Vec<String>, 
         // }
         i = i+1;
     }
-    Ok(return_vec)
- }
+    Ok((return_vec, i))
+}
+
 
  //This reads the db reference file to get the db connection info being used.
  pub fn get_db_url()-> String {
@@ -236,21 +254,22 @@ pub fn get_table_schema (current_connection: PooledConn) -> Result<Vec<String>, 
 //This preps the MySQL statement for a select transaction
  pub fn prep_unknown_Id_query (unknown_Id_table:String) -> String {
     let get_schema_stmt = format!("SELECT {}.Equipment_id from db_centraco3.{}", unknown_Id_table.trim(), unknown_Id_table.trim());
-    println!("The Schema statement---> {}\n", get_schema_stmt);
+    // println!("The Schema statement---> {}\n", get_schema_stmt);
     
     return get_schema_stmt;
  }
 
+ // COMMENT NEEDED!!!
  pub fn scrub_unknowns (current_connection:PooledConn, stmt:String) -> Vec<String> {
     let mut conn = current_connection;
-    println!("Query stmt is: {:?}", &stmt);
+    //println!("Query stmt is: {:?}", &stmt);
     let mut res:Vec<String> =  conn.query(stmt).unwrap();
     let tester = res.clone();
     //let return_vec:Vec<String>= vec![]; //just incase I have to append each iteration by pushing onto a vec...
         for r in tester.iter() {
-        println!("\nResult from query is: {}", r);
+        //println!("\nResult from query is a vector of {} Rows", r);                  //passed to run func fine, so this is not needed.
         }
-        println!("\nThis is tester in scrub unknowns Func: {:?}", tester);
+        //println!("\nThis is a test var in scrub unknowns Func: {:?}", tester);      //passed to run func fine, so this is not needed.
 
         if tester.len() < 2 {
             println!("# of unknown cars returned is: {}", tester.len());
@@ -289,8 +308,8 @@ pub fn get_table_schema (current_connection: PooledConn) -> Result<Vec<String>, 
         println!("{}", lognote);
         //settings::logthis(lognote); no need to log this atm
     }
-    println!("last ID sent to umler was: {}", last_unknown.clone());
+    //println!("last ID sent to umler was: {}", last_unknown.clone());
     last_unknown 
  }
 
-}
+
--- src/wsdl_send.rs
+++ src/wsdl_send.rs
@@ -7,58 +7,58 @@
 
 
 use std::env::var;
-use mysql::{PooledConn, TxOpts, prelude::Queryable, Error, from_value_opt, FromValueError, Row, from_row};
+use mysql::{PooledConn, TxOpts, prelude::Queryable, Error, from_value_opt, FromValueError, Row, from_row, Value};
 use mysql_common::*;
 //need to create a build.rs for this to work...A struct will need to be created for AEI tags in dbStructs.rs for this to work.
 use savon;
 use crate::settings;
 
 /* New idea 05/25 lets try pushing the returned values (Row) from the SQL query onto a newly created vector so then oving each element into its own vector
-then iterating over that, its convoluted and clunky but i think it will allow for addressing individual items in a row since MySQL Row can not implement iter() */
+then iterating over that, its convoluted and clunky but i think it will allow for addressing individual items in a row since MySQL Row can not implement iter()
+Its not ideal to pass desired order to mysql as I can't trust the order the queries are answered. would rather get an ordered vector or row returned so I can ensure 
+the right values coorespond to the right columns by iterating over each rows columns, problem is, MySQL doesn't implement Row as an iterator since-____-' */
 
 
 //The function below works but due to the for loop, will return only the first column to avoid excess info during development. This it Type: Vec<Rows>
-pub fn dummy_wsdl_send(db_connection:PooledConn, queryStmt: String, db_string:String) -> Result<Vec<String>, Error> {
+pub fn dummy_wsdl_send(db_connection:PooledConn, queryStmt: String, db_string:String, columnCount:i32) -> Result<Vec<String>, Error> {
     let mut conn = db_connection.unwrap();
     let mut selection = conn.start_transaction(TxOpts::default())?;
     let res:Vec<Row> = selection.query(queryStmt).unwrap();
     let mut return_vec:Vec<String>= vec![];
     let printable = res.clone();
+    let newattempt = res.clone();
     for row in res {
-    let conversion = row[1].clone();
-        let conversion = match from_value_opt::<String>(conversion){
-            Ok(string) => {
-                println!("Wsdl_send Column: {}", string);
-                return_vec.push(string);
-                // return Ok(()); //may remove, this was used in an example but may be a weird format thing
+            let row_length =  row.len();
+            for i in 0..row_length{
+                let conversion = row[i].clone();
+                //println!("data iterated per row?...{:?}", row[i]);
+                //attempt to convert
+                let conversion = match from_value_opt::<String>(conversion.clone()){
+                    Ok(string) => {
+                        println!("\nColumn value before: {:?}", conversion);
+                        println!("Column value after conversion: {}\n", string); //wrong order information, search term doesn't match equipment ID
+                        return_vec.push(string);
+                        // return Ok(()); //may remove, this was used in an example but may be a weird format thing
+                    }
+                    Err(e) => settings::logthis_dbRelated(e.to_string(), db_string.clone()) /*conversion*/,
+                };
+
             }
-            Err(e) => settings::logthis_dbRelated(e.to_string(), db_string.clone()) /*conversion*/,
-        };
+
+
+            println!("Row length is: {}", row_length);
+         
     }
     let mut i = 0;
     for row in printable.clone() {
-            let conversion2 = row.clone();
-            let conversion: String = mysql::from_value(row[i].clone());
-            //println!("\value is: {:?}\n", conversion2.clone());
-            println!("\nconversion element 1: {:?}\n", conversion2[0]);
-            println!("*!*!*!*! This is newTry Val: {:?}", conversion);
-
-            
-            // let conversion2 = match from_value_opt::<String>(){
-            //     Ok(string) => {
-            //         println!("Wsdl_send Data-Type: {}", string);
-            //         return_vec.push(string);
-            //         // return Ok(());
-            //     }
-            //     Err(FromValueError(conversion2)) => () /*conversion2*/,
-            // };
-        
-
+    
         i = i + 1;
     }
-    println!("\nWhats returned from query(res):\n {:?}\n", printable.clone());
-    println!("\nWhats returned from query(returned_vec): {:?}\n", return_vec.clone());
-    let joined = return_vec.clone().join("\n");
+
+
+    println!("\nWhats returned from query(res):\n {:?}\n", printable.clone()); //right info, wrong format(bytes) and blob so needs iterator 
+    //println!("\nWhats returned from query(returned_vec): {:?}\n", return_vec.clone()); //Not what I'm looking for.... this is the equipment ID's being searched
+    let joined = return_vec.clone().join(", ");
     println!("\nJoined is: {}\n", joined);
     Ok(return_vec)
 }
@@ -66,7 +66,7 @@ pub fn dummy_wsdl_send(db_connection:PooledConn, queryStmt: String, db_string:St
 
 
 pub fn db_statement_formatter(current_IDs:Vec<String>) -> String {
-    println!("Web Service Formatter functionality not added yet...");
+    println!("\n\n\n*******Database SEARCH statement acting as dummy Umler Webservice call*******\n\n\n\n");
 
     let unknown_IDs = current_IDs.clone();
     let mut search_stmt_vec = vec![];
@@ -89,9 +89,9 @@ pub fn db_statement_formatter(current_IDs:Vec<String>) -> String {
         }
 
 
-    println!("Search Statement Vec is: {:?}", search_stmt_vec);
+    //println!("Search Statement Vec is: {:?}", search_stmt_vec);      //commented since this is  working, left for debug purposes only
     let joined = search_stmt_vec.join("\n");
-    println!("joined and ready is : {}", joined);
+    println!("\nComplete Search statement : {}\n\n\n\n\n\n\n\n", joined);
  
     joined
 }
